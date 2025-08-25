df795660d1d809e25d316fb2a3694e4493002209
Luczak Bartosz <spoonofcode@gmail.com>
2025-08-25 15:19:34 +0200

    #5 Fix deploy

--- src/main/kotlin/com/spoonofcode/poa/Application.kt
+++ src/main/kotlin/com/spoonofcode/poa/Application.kt
@@ -9,12 +9,11 @@ fun main(args: Array<String>): Unit = EngineMain.main(args)
 fun Application.module() {
     configureMonitoring()
     configureSerialization()
-//    configureDI()
+    configureDI()
 //    configureDatabases()
 //    configureOAuthGoogleWebClient()
-//    configureAuthenticationJWT()
-//    configureRouting()
-    routingDocker()
+    configureAuthenticationJWT()
+    configureRouting()
 //    configureFirebase()
 
 
